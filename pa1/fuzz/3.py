8d2a000c9d5eab8315f49e6d0ba1140a6ec9cbd5
sakria9 <i@sustech.moe>
2023-06-12 21:19:05 +0800

    fuzz

---
+++ pa1/fuzz/3.py
@@ -0,0 +1,20 @@
+class b(object):
+    pass
+class c1_(object):
+    a:bool=False
+    def __init__(self:"c1_",a:bool):
+        self.a=a
+        print("Hello World")
+    def l49(self:"c1_")->bool:
+        return self.a
+M:c1_=None
+_:bool=True
+
+
+
+def J() -> b:
+    not True and True
+    while False and M.l49() :
+        True
+M=c1_(False)
+False if True else False if True if False else False else  not False if  not 1>=2 else False and _ if  not False or True else False or True if False else  not True
---
+++ pa2/fuzz/3.py
@@ -0,0 +1,17 @@
+class b(object):
+    pass
+class c1_(object):
+    a:bool=False
+    def l49(self:"c1_")->bool:
+        return self.a
+M:c1_=None
+_:bool=True
+
+
+
+def J() -> b:
+    not True and True
+    while False and M.l49() :
+        True
+M=c1_()
+False if True else False if True if False else False else  not False if  not 1>=2 else False and _ if  not False or True else False or True if False else  not True
